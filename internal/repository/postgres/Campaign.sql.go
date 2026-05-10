a0bd8a6af69aafc241c3fcbdf37e8c8758977c5a
alibek <alibek.yelgundiyev@gmail.com>
2026-05-10 20:27:56 +0500

    Fixed repository.go and added sqlStore

--- internal/repository/postgres/Campaign.sql.go
+++ internal/repository/postgres/Campaign.sql.go
@@ -3,7 +3,7 @@
 //   sqlc v1.31.1
 // source: Campaign.sql
 
-package repository
+package postgres
 
 import (
 	"context"
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,1 +1,18 @@
 package repository
+
+import (
+	"github.com/alibek2024/FundGo/internal/repository/postgres"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type SQLStore struct {
+	*postgres.Queries
+	Conn *pgxpool.Pool
+}
+
+func NewStore(conn *pgxpool.Pool) *SQLStore {
+	return &SQLStore{
+		Queries: postgres.New(conn),
+		Conn: conn,
+	}
+}
