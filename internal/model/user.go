776c556bc43c35de643cc4590469446912aa0257
alibek <alibek.yelgundiyev@gmail.com>
2026-05-15 19:37:26 +0500

    added funcs in wallet and refacor code in sql dir

--- internal/model/user.go
+++ internal/model/user.go
@@ -19,20 +19,25 @@ type User struct {
 }
 
 type UserInput struct {
-	ID           int32           `json:"id" db:"id"`
-	FirstName    string          `json:"firstName" db:"firstName"`
-	LastName     string          `json:"lastName" db:"lastName"`
-	Email        string          `json:"email" db:"email"`
-	HashPassword string          `json:"hashPassword" db:"hashPassword"`
-	UpdatedAt    time.Time       `json:"updatedAt" db:"updatedAt"`
+	ID           int32     `json:"id" db:"id"`
+	FirstName    string    `json:"firstName" db:"firstName"`
+	LastName     string    `json:"lastName" db:"lastName"`
+	Email        string    `json:"email" db:"email"`
+	HashPassword string    `json:"hashPassword" db:"hashPassword"`
+	UpdatedAt    time.Time `json:"updatedAt" db:"updatedAt"`
 }
 
 type UserResponse struct {
-	FirstName    string          `json:"firstName" db:"firstName"`
-	LastName     string          `json:"lastName" db:"lastName"`
-	Email        string          `json:"email" db:"email"`
-	ID           int32           `json:"id" db:"id"`
-	Balance      decimal.Decimal `json:"balance" db:"balance"`
-	CreatedAt    time.Time       `json:"createdAt" db:"createdAt"`
-	DeletedAt    time.Time       `json:"deletedAt" db:"deletedAt"`
+	FirstName string          `json:"firstName" db:"firstName"`
+	LastName  string          `json:"lastName" db:"lastName"`
+	Email     string          `json:"email" db:"email"`
+	ID        int32           `json:"id" db:"id"`
+	Balance   decimal.Decimal `json:"balance" db:"balance"`
+	CreatedAt time.Time       `json:"createdAt" db:"createdAt"`
+	DeletedAt time.Time       `json:"deletedAt" db:"deletedAt"`
+}
+
+type TopUpParams struct {
+	ID      int32
+	Balance decimal.Decimal
 }
