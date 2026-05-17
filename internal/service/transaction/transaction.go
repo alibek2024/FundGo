84a5eb73fceaa9262f04b2c20883297d0392201c
alibek <alibek.yelgundiyev@gmail.com>
2026-05-17 14:18:07 +0500

    commit changes

--- internal/service/transaction/transaction.go
+++ internal/service/transaction/transaction.go
@@ -1,11 +1,34 @@
 package transaction
 
 import (
+	"context"
+	"time"
+
 	"github.com/alibek2024/FundGo/internal/repository"
 )
 
 type TxService struct {
 	Store repository.Store
+}
+
+type CtxKey struct{}
+
+type Settings interface {
+	EnrichBy(external Settings) Settings
+	CtxKey() CtxKey
+	Propahgation()
+	Cancelable() bool
+	TimeOutOrNil() *time.Duration
+}
+
+type Manager interface {
+	DO(context.Context, func(context.Context) error) error
+
+	DoWithSettings(
+		context.Context,
+		Settings,
+		func(context.Context) error,
+	)
 }
 
 func CreateTX(db repository.Store) *TxService {
